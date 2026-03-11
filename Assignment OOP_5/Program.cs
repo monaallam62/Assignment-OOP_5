ac37ee041bdb7fe5522492f1fbfe8dc1dd050ea2
monaallam62 <monaallam5102@gmail.com>
2026-03-11 04:18:08 +0200

    Question 1

--- Assignment OOP_5/Program.cs
+++ Assignment OOP_5/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+            //Q1 : What is an interface in C#? Why do we use interfaces instead of depending on concrete classes directly? Mention at least three benefits of using interfaces.
+            // An interface defines a contract that a class must follow and defines a set of methods, properties, or events that a class must implement 
+            //It specifies what a class should do, but not how it should do it
+            //Enable polymorphism without inheritance ,remove tight coupling between classes,enable multiple inheritance
+            #endregion
         }
     }
 }
