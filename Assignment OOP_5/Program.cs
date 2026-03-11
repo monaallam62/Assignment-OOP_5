12d3d1a5e017fc2c9d018a1533d589a4ec87c171
monaallam62 <monaallam5102@gmail.com>
2026-03-11 04:47:43 +0200

    Question 2

--- Assignment OOP_5/Program.cs
+++ Assignment OOP_5/Program.cs
@@ -10,7 +10,23 @@
             // Loose Coupling,multiple Implementations,easier Testing
             //Enable polymorphism without inheritance ,remove tight coupling between classes,enable multiple inheritance
             #endregion
-
+            #region Question 2
+            //a) What is the problem with this design? Both interfaces have a method called Greet() — how does the class handle it currently?
+            //Both interfaces define a method called Greet(), but the Translator class provides only one implementation ,the class cannot provide different behavior for each interface
+            //==============================================================
+            //b) How would you fix this so IEnglishSpeaker.Greet() says "Hello" and IArabicSpeaker.Greet() says "Ahlan"? What is this technique called?
+            //We can fix it using Explicit Interface Implementation,This technique allows a class to provide different implementations for methods with the same name from different interfaces
+            //Technique name: Explicit Interface Implementation
+            //==============================================================
+            //c) After applying your fix, can you call Greet() directly on a Translator object (e.g. translator.Greet())? Why or why not? How do you call each version?
+            //No we cannot call it directly Because the methods are implemented explicitly, they are only accessible through the interface type
+            //Calling the English version
+            //IEnglishSpeaker eng = new Translator();
+            //eng.Greet(); // Hello
+            //Calling the Arabic version
+            //IArabicSpeaker ar = new Translator();
+            //ar.Greet(); // Ahlan
+            #endregion
         }
     }
 }
