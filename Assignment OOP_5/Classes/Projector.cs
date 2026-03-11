3ba2f22dbcaef4e6868225eaed1b511a429efd67
monaallam62 <monaallam5102@gmail.com>
2026-03-11 06:27:01 +0200

    Sealed Class Projector

---
+++ Assignment OOP_5/Classes/Projector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_OOP_5.Classes
+{
+    #region Sealed Class Projector
+    //public sealed class Projector
+    //{
+    //    public void Start()
+    //    {
+    //        Console.WriteLine("=== Cinema Opened ===");
+    //    }
+
+    //    public void Stop()
+    //    {
+    //        Console.WriteLine("\n=== Cinema Closed ===");
+    //    }
+    //}
+    #endregion
+}
